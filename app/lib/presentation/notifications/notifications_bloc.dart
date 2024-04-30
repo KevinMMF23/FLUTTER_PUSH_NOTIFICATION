48912adb5994070571012a6989390458faa6316a
kevin <kevin>
2024-04-29 21:26:28 -0700

    actualizacion

--- app/lib/presentation/notifications/notifications_bloc.dart
+++ app/lib/presentation/notifications/notifications_bloc.dart
@@ -7,8 +7,33 @@ part 'notifications_state.dart';
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   NotificationsBloc() : super(const NotificationsState()) {
-    // on<NotificationsEvent>((event, emit) {
-    //   // TODO: implement event handler
-    // });
+    _initializeFirebaseMessaging();
+  }
+
+  void _initializeFirebaseMessaging() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
+  @override
+  Stream<NotificationsState> mapEventToState(NotificationsEvent event) async* {
+    // Aquí puedes implementar la lógica relacionada con los eventos
   }
 }
