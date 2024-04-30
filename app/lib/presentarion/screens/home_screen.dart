b2652a92c89cb6c69e3f14b635046e47c8565634
unknown <kevin>
2024-04-29 23:30:21 -0700

    29/04/2024

---
+++ app/lib/presentarion/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_push_notification_2024_1/presentarion/notifications/notifications_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: context
+            .select((NotificationsBloc bloc) => Text('${bloc.state.status}')),
+        actions: [
+          IconButton(
+              onPressed: () {
+                context.read<NotificationsBloc>().requestPermission();
+              },
+              icon: const Icon(Icons.settings))
+        ],
+      ),
+      body: const Center(
+        child: Text("hola mundo"),
+      ),
+    );
+  }
+}
